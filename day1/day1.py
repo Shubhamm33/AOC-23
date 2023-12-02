809050f5277e395a3318cafa7c0cbbee77504a2c
Shubhamm33 <mohite.shubhu61@gmail.com>
2023-12-02 17:27:36 +0530

    updaet: Day1

--- day1/day1.py
+++ day1/day1.py
@@ -6,6 +6,7 @@ Author: @Shubhamm33 (github)
 import os
 from logger import logger
 
+# utils---
 def remove_new_line_character(val:str):
     newline_character = str(val).endswith("\n")
     if newline_character:
@@ -20,7 +21,7 @@ def get_first_last_digit(val:str):
     for each_val in val:
         if each_val.isdigit():
             digits.append(each_val)
-    logger.info(digits)
+    # logger.info(digits)
 
     if len(digits) == 0:
         return None
@@ -37,7 +38,51 @@ def get_first_last_digit(val:str):
     
     return digits
 
-def solve_puzzle():
+def evaluate_str(val):
+    # logger.info(val)
+
+    digits = []
+
+    string_representation = {
+        'one': 'o1e',
+        'two': 't2o',
+        'three': 't3e',
+        'four': 'f4r',
+        'five': 'f5e',
+        'six': 's6x',
+        'seven': 's7n',
+        'eight': 'e8t',
+        'nine': 'n9e',
+        'zero': 'z0o',
+    }
+
+    for key, value in string_representation.items():
+        if key in val:
+            val = str(val).replace(key, value)
+    
+    for each_val in val:
+        if each_val.isdigit():
+            digits.append(each_val)
+            
+    if len(digits) == 0:
+        return None
+    
+    elif len(digits) == 1:
+        num = digits[0] + digits[0]
+        num = int(num)
+        return num
+    
+    elif len(digits) > 1:
+        num = digits[0] + digits[-1]
+        num = int(num)
+        return num
+    
+    return digits
+
+# ---
+
+
+def solve_puzzle_part_1():
     path = os.path.join(os.getcwd(), 'day1.txt')
 
     with open(path, 'r') as f:
@@ -45,13 +90,26 @@ def solve_puzzle():
     
     lines = list(map(remove_new_line_character , lines))
     digits = list(map(get_first_last_digit, lines))
-    logger.info(digits)
+    # logger.info(digits)
 
     final_sum_of_calibration = sum(digits)
-    logger.info(f"Final sum of calibration: {final_sum_of_calibration}")
+    logger.info(f"Final sum of calibration Q1: {final_sum_of_calibration}")
+
+def solve_puzzle_part_2():
+    path = os.path.join(os.getcwd(), 'day1.txt')
+    # path = os.path.join(os.getcwd(), 'test.txt')
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
     
+    lines = list(map(remove_new_line_character , lines))
+    digits = list(map(evaluate_str, lines))
+    final_sum_of_calibration = sum(digits)
+    # logger.info(digits)
+    logger.info(f"Final sum of calibration Q2: {final_sum_of_calibration}")
 
 
 
 if __name__ == '__main__':
-    solve_puzzle()
+    solve_puzzle_part_1()
+    solve_puzzle_part_2()
